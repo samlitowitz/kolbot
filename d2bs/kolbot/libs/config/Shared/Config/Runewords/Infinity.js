b385e79de7133e78161182d34c38aaaec6249d89
Sam L <samlitowitz@users.noreply.github.com>
2023-08-05 10:35:57 -0400

    Update organization

--- d2bs/kolbot/libs/config/Shared/Config/Runewords/Infinity.js
+++ d2bs/kolbot/libs/config/Shared/Config/Runewords/Infinity.js
@@ -8,18 +8,20 @@ if (!isIncluded('common/Equip.js')) {
 }
 
 
-(function (module) {
-	module.exports.RollAndKeep = function () {
-		const merc = me.getMerc();
-		const hasInfinity = Equip.hasRunewordEquippedAt(merc, sdk.body.LeftArm, "Infinity");
+(function () {
+	return {
+		RollAndKeep: function () {
+			const merc = me.getMerc();
+			const hasInfinity = Equip.hasRunewordEquippedAt(merc, sdk.body.LeftArm, "Infinity");
 
-		if (hasInfinity) {
-			return false;
+			if (hasInfinity) {
+				return false;
+			}
+
+			Config.Recipes.push([Recipe.Socket.Weapon, "giantthresher", Roll.All]);
+			Config.Recipes.push([Recipe.Socket.Weapon, "thresher", Roll.All]);
+			Config.KeepRunewords.push("[type] == polearm && [class] == elite && [flag] == runeword && [flag] == ethereal # [meditationaura] >= 12");
+			return true;
 		}
-
-		Config.Recipes.push([Recipe.Socket.Weapon, "giantthresher", Roll.All]);
-		Config.Recipes.push([Recipe.Socket.Weapon, "thresher", Roll.All]);
-		Config.KeepRunewords.push("[type] == polearm && [class] == elite && [flag] == runeword && [flag] == ethereal # [meditationaura] >= 12");
-		return true;
 	};
-})(module);
+})();
--- d2bs/kolbot/libs/config/Shared/Config/Runewords/Insight.js
+++ d2bs/kolbot/libs/config/Shared/Config/Runewords/Insight.js
@@ -8,31 +8,33 @@ if (!isIncluded('common/Equip.js')) {
 }
 
 
-(function (module) {
-	module.exports.RollAndKeep = function () {
-		const merc = me.getMerc();
-		const hasInsight = Equip.hasRunewordEquippedAt(merc, sdk.body.LeftArm, "Insight");
+(function () {
+	return {
+		RollAndKeep: function () {
+			const merc = me.getMerc();
+			const hasInsight = Equip.hasRunewordEquippedAt(merc, sdk.body.LeftArm, "Insight");
 
-		// Any Insight is better than no Insight
-		if (!hasInsight) {
-			Config.Recipes.push([Recipe.Socket.Weapon, "polearm", Roll.All]);
-			Config.KeepRunewords.push("[type] == polearm && [flag] == runeword # [meditationaura] >= 12");
+			// Any Insight is better than no Insight
+			if (!hasInsight) {
+				Config.Recipes.push([Recipe.Socket.Weapon, "polearm", Roll.All]);
+				Config.KeepRunewords.push("[type] == polearm && [flag] == runeword # [meditationaura] >= 12");
+				return true;
+			}
+
+			// Only do upgrades
+			const mercWeapon = Equip.equippedAt(merc, sdk.body.LeftArm);
+			const hasEliteInsight = mercWeapon.itemclass === sdk.class.Elite,
+				hasEtherealInsight = mercWeapon.getFlag(sdk.items.flags.Ethereal)
+			;
+
+			if (hasEliteInsight && hasEtherealInsight) {
+				return false;
+			}
+			Config.Recipes.push([Recipe.Socket.Weapon, "giantthresher", Roll.All]);
+			Config.Recipes.push([Recipe.Socket.Weapon, "greatpoleaxe", Roll.All]);
+			Config.Recipes.push([Recipe.Socket.Weapon, "thresher", Roll.All]);
+			Config.KeepRunewords.push("[type] == polearm && [class] == elite && [flag] == runeword && [flag] == ethereal # [meditationaura] >= 12");
 			return true;
 		}
-
-		// Only do upgrades
-		const mercWeapon = Equip.equippedAt(merc, sdk.body.LeftArm);
-		const hasEliteInsight = mercWeapon.itemclass === sdk.class.Elite,
-			hasEtherealInsight = mercWeapon.getFlag(sdk.items.flags.Ethereal)
-		;
-
-		if (hasEliteInsight && hasEtherealInsight) {
-			return false;
-		}
-		Config.Recipes.push([Recipe.Socket.Weapon, "giantthresher", Roll.All]);
-		Config.Recipes.push([Recipe.Socket.Weapon, "greatpoleaxe", Roll.All]);
-		Config.Recipes.push([Recipe.Socket.Weapon, "thresher", Roll.All]);
-		Config.KeepRunewords.push("[type] == polearm && [class] == elite && [flag] == runeword && [flag] == ethereal # [meditationaura] >= 12");
-		return true;
 	};
-})(module);
+})();
